02b8676ab06e80f0f3ca1453037f3fcf2f517a38
Aksd-M/S <131961097+Ko-Song@users.noreply.github.com>
2026-08-21 09:38:46 +0800

    Update MainHook.java

--- app/src/main/java/com/example/wechatshield/MainHook.java
+++ app/src/main/java/com/example/wechatshield/MainHook.java
@@ -1,1 +1,13 @@
+package com.example.wechatshield;
 
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.startsWith("com.tencent.mm")) {
+            return;
+        }
+    }
+}
